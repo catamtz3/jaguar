c0e691d4695cab07af08ea21abaa6c03e42fee57
catamtz3 <catamtz3@cs.washington.edu>
2022-05-10 20:43:10 -0700

    error quicksort

--- src/main/java/p2/sorts/QuickSort.java
+++ src/main/java/p2/sorts/QuickSort.java
@@ -13,24 +13,18 @@ public class QuickSort {
         if (comparator.compare(array[0], array[array.length]) < 0){
             int start = 0;
             int end = array.length;
-            int pivot = pivot(array, start, end);
-            pivot()
-            sort(array, comparator.compare(start, end))
-        }
-    }
-
-    public static int pivot(E[] array, int start, int end){
-        int pivot = start + end / 2;
-        E pivotValue = array[pivot];
-        while (true){
-            do start++; while(comparator.compare(array[start], pivotValue) < 0);
-            do end--; while(comparator.compare(array[end], pivotValue) > 0);
-            if (start >= end){
-                end;
+            int pivot = start + end / 2;
+            E pivotValue = array[pivot];
+            while (true){
+                do start++; while(comparator.compare(array[start], pivotValue) < 0);
+                do end--; while(comparator.compare(array[end], pivotValue) > 0);
+                if (start >= end){
+                    break;
+                }
+                E temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
             }
-            E temp = array[start];
-            array[start] = array[end];
-            array[end] = temp;
         }
     }
 }
